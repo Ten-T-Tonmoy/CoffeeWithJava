4f50887557bb56d211e24dc223354a6758307c13
Ten-T-Tonmoy <stu01754@gmail.com>
2025-03-15 01:05:53 +0600

    using jetbrains

---
+++ learningBrocode/src/syntax.java
@@ -0,0 +1,9 @@
+public class syntax {
+    public static void main(String[] args){
+        //well comments are same anyways
+        System.out.println("curse u griffith");
+        //umm yea does shit println and print
+
+        System.out.print("again ");
+    }
+}
